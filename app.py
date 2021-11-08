41072e2057c40b53421ada965a096129db54a6f0
Keshava Prasaath <85608027+Keshav25-2002@users.noreply.github.com>
2021-11-08 17:46:48 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -29,13 +29,13 @@ def result():
     X = np.array([[item_weight, item_fat_content, item_visibility, item_type, item_mrp,
                    outlet_establishment_year, outlet_size, outlet_location_type, outlet_type]])
 
-    scaler_path = r'C:\Users\Lenovo\Desktop\internshipfile\sc.sav'
+    scaler_path = r'sc.sav'
 
     sc = joblib.load(scaler_path)
 
     X_std = sc.transform(X)
 
-    model_path = r'C:\Users\Lenovo\Desktop\internshipfile\rf.sav'
+    model_path = r'rf.sav'
 
     model = joblib.load(model_path)
 
